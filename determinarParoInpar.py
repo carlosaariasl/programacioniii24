4d6fe37f0ba669f48395a00f3879cabf6672e746
Carlos Arias <43482712+carlosaariasl@users.noreply.github.com>
2024-01-23 11:44:14 -0600

    Add files via upload

---
+++ determinarParoInpar.py
@@ -0,0 +1,10 @@
+numero = int(input("Ingresa un numero: "))
+
+if numero % 2 == 0:
+    print("el numero es par")
+else:
+    print("el numero es impar")
+
+
+
+
---
+++ programa.py
@@ -0,0 +1,23 @@
+edad = 38
+nombre = "34"
+caracter = '@'
+boleano = True
+flotante = 23.2
+
+a = 10
+b = 5
+
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+division = a / b
+potencia = a ** b
+modulo = a % b
+
+print("Las operaciones aritmeticas en python dan como resultado: Para suma", suma, "Resta:",
+      resta, "Multiplicacion:", multiplicacion, "Divisin:", division, "Potencia:", potencia)
+
+sumarTiposvariados = edad + float(nombre)
+print(" Tipos: ", sumarTiposvariados)
+
+
